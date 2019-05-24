5e226519a42ba71f6a13969ec2b586e8c6ee7948
Ida <ida@mba-5275.local>
2019-05-24 18:52:32 +0300

    solver intial commit

--- MineSweeper/src/main/java/tira/minesweeper/solver/Solver.java
+++ MineSweeper/src/main/java/tira/minesweeper/solver/Solver.java
@@ -5,10 +5,23 @@
  */
 package tira.minesweeper.solver;
 
+import java.util.ArrayList;
+import tira.minesweeper.logic.Coordinate;
+import tira.minesweeper.logic.Field;
+
 /**
  *
  * @author ida
  */
 public class Solver {
     
+    /***
+     * Finds block of unopened fields whose mine status is independent of unopened fields outside the block
+     * @return Fields within the block
+     */
+    public ArrayList<Field> commonBlock() {
+        ArrayList<Field> block = new ArrayList<>();
+        //to-do
+        return block;
+    }    
 }
