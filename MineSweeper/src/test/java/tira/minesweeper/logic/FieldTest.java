cd1220bf6925546f283a79b1056621ca0b6a0b47
Ida <ida@dsl-hkibng32-54fb42-148.dhcp.inet.fi>
2019-06-07 21:51:34 +0300

    tests

---
+++ MineSweeper/src/test/java/tira/minesweeper/logic/FieldTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tira.minesweeper.logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ida
+ */
+public class FieldTest {
+    
+    Field field;
+   
+    @Before
+    public void setUp() {
+        field = new Field(new Coordinate(0, 0));
+    }
+
+    @Test
+    public void equalsFieldsWorks1() {
+        assertTrue(field.equals(new Field(new Coordinate(0, 0))));
+    }
+    @Test
+    public void equalsFieldsWorks2() {
+        assertFalse(field.equals(new Field(new Coordinate(1, 0))));
+    }
+}
---
+++ MineSweeper/src/test/java/tira/minesweeper/solver/SolverTest.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tira.minesweeper.solver;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tira.minesweeper.logic.Board;
+import tira.minesweeper.logic.Field;
+
+/**
+ *
+ * @author ida
+ */
+public class SolverTest {
+    
+    Solver solver;
+    
+    @Before
+    public void setUp() {
+        // Board setup
+        // [M M 1 0]
+        // [2 2 1 0]
+        // [0 0 1 1]
+        // [0 1 2 M]
+        // [0 1 M 2]
+        solver = new Solver(5, 4, 0);
+        solver.board = new Board(5, 4);
+        
+        //set four mines in x-y coordinates (0,0), (1,0), (3,4) and (2,4)
+        solver.board.setMine(solver.board.getFieldAt(0, 0));
+        solver.board.setMine(solver.board.getFieldAt(1, 0));
+        solver.board.setMine(solver.board.getFieldAt(3, 3));
+        solver.board.setMine(solver.board.getFieldAt(2, 4));
+        
+        solver.board.placeNumbers();
+        
+                
+        
+        
+        //open square (1,2)
+        solver.board.openField(solver.board.getFieldAt(1, 2));
+
+    }
+    
+    
+    @Test
+    public void countsEffectiveNumberCorrectly1() {
+        assertEquals(2, solver.effectiveNumber(2, 3));
+    }
+    
+    @Test
+    public void countsEffectiveNumberCorrectly2() {
+        solver.board.setFlag(solver.board.getFieldAt(2, 4));
+        assertEquals(1, solver.effectiveNumber(2, 3));
+    }
+    
+    @Test
+    public void isAFNworks1() {
+        assertFalse(solver.isAFN(2, 3));
+    }
+    @Test
+    public void isAFNworks2() {
+        solver.board.setFlag(solver.board.getFieldAt(2, 4));
+        solver.board.setFlag(solver.board.getFieldAt(3, 3));
+        assertTrue(solver.isAFN(2, 3));
+    }
+    @Test
+    public void isAMNworks1() {
+        solver.board.setFlag(solver.board.getFieldAt(2, 4));
+        solver.board.setFlag(solver.board.getFieldAt(3, 3));
+        assertFalse(solver.isAMN(2, 3));
+    }
+    @Test
+    public void isAMNworks2() {
+        solver.board.openField(solver.board.getFieldAt(3, 2));
+        solver.board.openField(solver.board.getFieldAt(3, 4));
+        assertTrue(solver.isAMN(2, 3));
+    }
+    @Test
+    public void getUnflaggedNeighboursWorks() {
+        solver.board.setFlag(solver.board.getFieldAt(3, 3));
+        ArrayList<Field> unflaggedNeighbours = solver.getUnFlaggeddNeighbours(2, 3);
+        assertTrue(unflaggedNeighbours.contains(solver.board.getFieldAt(3, 2)));
+        assertTrue(unflaggedNeighbours.contains(solver.board.getFieldAt(2, 4)));
+        assertTrue(unflaggedNeighbours.contains(solver.board.getFieldAt(3, 4)));
+        assertEquals(3, unflaggedNeighbours.size());
+    }
+    
+}
