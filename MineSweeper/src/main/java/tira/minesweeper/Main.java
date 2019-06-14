42e5ebe8d78a32e6259ac2eadb34cc924a2af3a5
Ida <ida@dsl-hkibng32-54f847-31.dhcp.inet.fi>
2019-06-14 22:54:44 +0300

    performance testing

--- MineSweeper/src/main/java/tira/minesweeper/Main.java
+++ MineSweeper/src/main/java/tira/minesweeper/Main.java
@@ -5,12 +5,34 @@
  */
 package tira.minesweeper;
 
+import tira.minesweeper.measurement.Performance;
+
+
+
+
 /**
  *
  * @author ida
  */
 public class Main {
-
-    public static void main(String[] args) {     
+    
+    public static void main(String[] args) {
+        //to-do: rows,cols and mines are hardcoded, ask rather from user.
+        int rows = 16;
+        int cols = 16;
+        int mines = 40;
+        
+        Performance performance = new Performance(rows, cols, mines);
+        performance.doCalculations();
+        double failed = performance.getFailed();
+        double solved = performance.getSolved();
+        int repetitions = performance.getRepetition();
+        long execTime = performance.getTime();
+        
+        //there is a bug 
+        System.out.println(failed + "% are failed");
+        System.out.println(solved + "% are solved");
+        System.out.println(execTime + " ms for " + repetitions + " games");
+                
     }    
 }
---
+++ MineSweeper/src/main/java/tira/minesweeper/measurement/Performance.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tira.minesweeper.measurement;
+
+import tira.minesweeper.solver.Solver;
+
+/**
+ *
+ * @author ida
+ */
+public class Performance {
+    private int rows,cols, mines;
+    private int failed, solved;
+    private int repetition;
+    private long time;
+    private Solver solver;
+
+    public Performance(int rows, int cols, int mines) {
+        
+        this.rows = rows;
+        this.cols = cols;
+        this.mines = mines;
+        this.failed = 0;
+        this.solved = 0;
+        this.repetition = 1000;
+        this.time = 0;
+    
+    }
+    
+    public void doCalculations() {
+        long startTime = System.currentTimeMillis();
+        
+        int i = 0;
+        
+        while (i < repetition) {
+            solver = new Solver(rows, cols, mines);
+            solver.init();
+        
+            while (!solver.board.isFailed() && !solver.board.isSolved()) {
+                solver.DSSP();
+            }
+
+            if (solver.board.isFailed()) {
+                failed++;
+            }
+            if (solver.board.isSolved()) {
+                solved++;
+            }
+            
+            i++;
+        }
+        
+        long endTime = System.currentTimeMillis();
+        
+        time = endTime - startTime;
+    }
+
+    
+    
+    public double getFailed() {
+        return  failed * 100.0 / repetition;
+    }
+
+    
+    
+    public double getSolved() {
+        return solved * 100.0 / repetition;
+
+    }
+
+    
+    
+    public long getTime() {
+        return time;
+    }
+
+    
+    
+    public int getRepetition() {
+        return repetition;
+    }
+     
+         
+        
+}
